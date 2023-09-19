98e2f5c4e95d4b2a6554d0c1fb85c482f6f3f4d9
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-09-19 16:44:17 -0700

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+Config.robberyTime = 5
+Config.rewardAmount = 1000
+Config.maxDistance = 30.0
+Config.robberyCooldown = 200
+
+Config.stores = {
+    {name = "Black Water Bank", x = -817.25, y = -1275.02, z = 43.64},
+ 
+}
