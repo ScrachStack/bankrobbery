e2462f4fa5343658e7f93b1d4b7340103dd239b4
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-09-19 17:36:46 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -7,5 +7,6 @@ Config.robberyCooldown = 200
 
 Config.stores = {
     {name = "Black Water Bank", x = -817.25, y = -1275.02, z = 43.64},
- 
+     {name = "Saint Denis  Bank", x = 2642.59, y = -1305.51, z = 52.25},
+-- Copy and paste sanit denis and edit coords and name for more 
 }
