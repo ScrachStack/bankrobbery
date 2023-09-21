7dfbe5d81c47fbe8e4e43741191cad7214cf87db
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-09-20 21:41:18 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,7 +4,7 @@ Config.robberyTime = 5
 Config.rewardAmount = 1000
 Config.maxDistance = 30.0
 Config.robberyCooldown = 200
-
+Config.ItemRequirement = 'dynamite'
 Config.stores = {
     {name = "Black Water Bank", x = -817.25, y = -1275.02, z = 43.64},
      {name = "Saint Denis  Bank", x = 2642.59, y = -1305.51, z = 52.25},
