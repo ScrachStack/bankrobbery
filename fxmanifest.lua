d3975c6d466a98dceae77c23084ec2f8a0b8f2e5
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-09-22 00:13:57 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@
 games { 'rdr3' }
 fx_version 'adamant'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
-version '1.0'
+version '1.5'
 client_scripts {
 'client/*'
 }
